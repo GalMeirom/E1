dfbe5b28e3f55556ed58d87d2b58b4f44aeb79b5
Gal Meirom <galmeirom@gmail.com>
2024-01-31 19:28:08 +0200

    Finishing Q1

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import scipy
 import utils as ut
 import matplotlib.pyplot as plt
 
-def Q1C1():
-    mat = scipy.io.loadmat('data/SwissRollData.mat')
+##Q1
+
+def Q1C1(data):
+    mat = scipy.io.loadmat('data/'+data+'.mat')
     Ct = mat['Ct']
     Xt = mat['Yt']
     W = ut.genRandArr(Xt.shape[0],Ct.shape[0])
-    softmax = ut.softmaxLoss(Xt,W,Ct)
+    params = [Xt,W,Ct]
+    softmax = ut.softmaxLoss(params)
     print(f'This is Soft-Max: {softmax}')
     x = []
     Yt0 = []
@@ -16,7 +19,7 @@ def Q1C1():
     d = ut.genRandNormArr(W.shape[0],W.shape[1])
     for e in range(1, 200 ,1):
         t0 = ut.zeroTaylor(Xt,W,Ct, 0.1*e, d)
-        t1 = ut.gradTest(Xt,W,Ct, 0.1*e, d)
+        t1 = ut.gradTest(params, ut.softmaxLoss, ut.calculateSMGradW, 0.1*e, d)
         x.append(e)
         Yt0.append(t0)
         Yt1.append(t1)
@@ -29,11 +32,72 @@ def Q1C1():
     plt.show()
 
 def Q1C2():
-    mat = scipy.io.loadmat('data/SwissRollData.mat')
+    A = ut.genRandArr(8,6)
+    X = ut.genRandArr(6,7)
+    b = ut.genRandArr(8,7)
+    params = [A, X, b]
+    x = []
+    err = []
+    for j in range(1, 15):
+        err.append(ut.linearLeastSquares(params))
+        x.append(j)
+        params = ut.SGD(ut.calculateLLSGradX, params, 0.1)
+    plt.plot(x, err ,color = 'blue')
+    plt.xlabel('Number of Iterations')
+    plt.ylabel('Linear Least Squares Error')
+    plt.title('Testing Stochastic Gradient Descent')
+    plt.legend()
+    plt.show()
+    
+
+def Q1C3(data):
+    mat = scipy.io.loadmat('data/' + data + '.mat')
     Ct = mat['Ct']
     Xt = mat['Yt']
+    Cv = mat['Cv']
+    Xv = mat['Yv']
+    batchS = 400
+    numEpoch = 10
+    xGraph = []
+    yGraphPrecent = []
+    yGraphSoftMax = []
     W = ut.genRandArr(Xt.shape[0],Ct.shape[0])
-    softmax = ut.softmaxLoss(Xt,W,Ct)
-    t = ut.gradTest(Xt,W,Ct, 0.001, ut.genRandNormArr(Xt.shape[0],Xt.shape[1]))
+    params = [Xt,W,Ct]
+    for epoch in range(1, numEpoch):
+        xGraph.append(epoch)
+        for i in range(1, 1000):
+            params = ut.SGD(ut.calculateSMGradW, params, 0.1)
+        batchPrecent = 0
+        selected_indices = np.random.choice(Xt.shape[1], batchS, replace=False)
+        batchXt = Xt[:, selected_indices]
+        batchCt = Ct[:, selected_indices]
+        batchRes = np.matmul(np.transpose(W), batchXt)
+        for j in range(1, batchS):
+            if batchCt[np.argmax(batchRes[:, j]), j] == 1:
+               batchPrecent = batchPrecent + 1
+        yGraphPrecent.append(100*batchPrecent/batchS)
+        yGraphSoftMax.append(ut.softmaxLoss([batchXt, W, batchCt]))
+    batchPrecent = 0
+    selected_indices = np.random.choice(Xv.shape[1], batchS, replace=False)
+    batchXv = Xv[:, selected_indices]
+    batchCv = Cv[:, selected_indices]
+    batchResV = np.matmul(np.transpose(W), batchXv)
+    for m in range(1, batchS):
+        if batchCv[np.argmax(batchResV[:, m]), m] == 1:
+            batchPrecent = batchPrecent + 1
+    xGraph.append(numEpoch)
+    yGraphPrecent.append(100*batchPrecent/batchS)
+    yGraphSoftMax.append(ut.softmaxLoss([batchXv, W, batchCv]))
+    plt.scatter(xGraph, yGraphPrecent, c='blue', marker='o', s=50, edgecolors='black', alpha=0.7)
+    plt.scatter(xGraph[numEpoch-1], yGraphPrecent[numEpoch-1], c='red', marker='o', s=50, edgecolors='black', alpha=0.7)
+    plt.text(xGraph[numEpoch-1], yGraphPrecent[numEpoch-1], "Test Batch", fontsize=8, ha='right', va='bottom')
+    plt.xlabel('Amount of ephocs')
+    plt.ylabel('Precent of accuracy')
+    plt.title('Precent from different batches by different ephocs')
+    plt.show()
 
-Q1C1()
+            
+Data = ['SwissRollData','PeaksData','GMMData']            
+Q1C3(Data[2])
+
+
--- utils.py
+++ utils.py
@@ -1,20 +1,35 @@
 import numpy as np
 
-
-def softmaxLoss(X,W,C):
+#### Functions
+def softmaxLoss(params):
     # Calculate the Soft-Max loss function according to X with W weights and indicator C
     # Assumptions:
-        # X is : (t,m) 
-        # C is : (l,m)
-        # W is : (t,l)
- 
+        # params[0] = X is : (t,m) 
+        # params[1] = W is : (t,l)
+        # params[2] = C is : (l,m)
+    X = params[0]
+    W = params[1]
+    C = params[2]
     F = 0
     for k in range (C.shape[0]):  # C.shape is supposed to be the number of classes
         F += np.matmul(np.transpose(C[k, :]) , np.log(calculateDiag(X,W,k)))
     
     F = (-1/(X.shape[1])) * F
     return F
-    
+
+def linearLeastSquares(params):
+    # Calculate the Linear Lest Squares loss function according to X, A and B bias
+    # Assumptions:
+        # params[0] = A is : (n,m) 
+        # params[1] = X is : (m,t)
+        # params[2] = b is : (n,t)
+    A = params[0]
+    X = params[1]
+    b = params[2]
+    return np.linalg.norm(np.matmul(A, X) - b, 2)*0.5
+
+
+## Satellite methods for Losses
 
 def calculateDiag(X,W,k):
 
@@ -30,24 +45,46 @@ def calculateDiag(X,W,k):
     return J
     
 
+## Gradient Functions
 
-def calculateSoftMaxGradW(X,W,C):
+def calculateSMGradW(params):
     # Calculate the Soft-Max gradient by W,
     #   according to X with W weights, indicator C.
     # Assumptions:
-        # X is : (t,m) 
-        # C is : (l,m)
-        # W is : (t,l)
+        # params[0] = X is : (t,m) 
+        # params[1] = W is : (t,l)
+        # params[2] = C is : (l,m)
+    X = params[0]
+    W = params[1]
+    C = params[2]
     grad = 1/X.shape[1] *np.matmul(X,calculateDiagGrad(X,W,C))
     return grad
 
-    
+def calculateLLSGradX(params):
+    # Calculate the LLS gradient by X,
+    #   according to X with A, and b bias.
+    # Assumptions:
+        # params[0] = A is : (n,m) 
+        # params[1] = X is : (m,t)
+        # params[2] = b is : (n,t)
+    A = params[0]
+    X = params[1]
+    b = params[2]
+    return np.matmul(np.matmul(np.transpose(A), A), X) - np.matmul(np.transpose(A), b)
+
+
+## Satellite methods for Gradients Functions
+
+
 def calculateDiagGrad(X,W,C):
     res = []
     for p in range(W.shape[1]):
         Jp = calculateDiag(X, W, p).reshape(-1, 1)
         res.append(Jp)
     return np.hstack(res)
+
+
+## Convergence Tests
 
 def zeroTaylor(X, W, C , eps, d):
     # Calculate gradient test
@@ -58,19 +95,23 @@ def zeroTaylor(X, W, C , eps, d):
         # W is : (t,l)
         # d is : (t,m)
 
-    return np.linalg.norm(abs(softmaxLoss(X, W + eps * d, C) - softmaxLoss(X, W, C)))
+    return np.linalg.norm(abs(softmaxLoss([X, W + eps * d, C]) - softmaxLoss([X, W, C])))
 
 
-def gradTest(X, W, C , eps, d):
+def gradTest(params, func, gradFunc, eps, d):
     # Calculate gradient test
-    #   according to X with W weights, indicator C, eps number, d random generated normalized matrix.
+    #   according to parameters of a function, function f, gradient function of f,
+    #    some small epsilon and random generated normalized matrix d.
     # Assumptions:
         # X is : (t,m) 
         # C is : (l,m)
         # W is : (t,l)
         # d is : (t,m)
+    shift = params.copy()
+    shift[1] = shift[1] + eps * d
+    return np.linalg.norm(abs(func(shift) - func(params) - eps * np.matmul(np.transpose(d),gradFunc(params))))
 
-    return np.linalg.norm(abs(softmaxLoss(X, W + eps * d, C) - softmaxLoss(X, W, C) - eps * np.matmul(np.transpose(d),calculateSoftMaxGradW(X, W, C))))
+## Utils
 
 def genRandArr(rows, cols):
     return np.random.rand(rows,cols)
@@ -80,8 +121,16 @@ def genRandNormArr(rows, cols):
     return d/np.linalg.norm(d)
 
 
-#def SGD(W,learning_rate=0.01 , beta ):
-#    W = W - learning_rate * #grad W
+## Optimizations
+
+
+def SGD(gradFunc, params, LR):
+    step = params.copy()
+    step[1] = step[1] - LR * gradFunc(params)
+    return step
     
+     
+
+## Plots
 
 
